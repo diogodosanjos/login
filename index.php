7fde5458eaa1d411dc96ac0f4772439f4144a128
Diogo dos Anjos <dodogeremias@gmail.com>
2019-09-24 21:28:19 -0300

    Formulário de recuperação de senha
    close #2

--- index.php
+++ index.php
@@ -61,22 +61,65 @@
                 </div>
             </div>
 
-                <div class="form-group">
+                    <div class="form-group">
                     <input type="submit" value="::Entrar::" name="btnEntrar"
                     id="btnEntrar" class="btn btn-primary btn-block">
-                </div>
+                    </div>
 
-                <div class="form-group">
-                    <p class="text-center">
+                    <div class="form-group">
+                        <p class="text-center">
                         Novo usuário?<a href="#" id="btnRegistrar">
                         Registre-se aqui!
-                    <a/>
-                </p>
-            </div>
+                        <a/>
+                    </p>
+                </div>
 
-            </form>
-        </div>
-    </section>
+                </form>
+            </div>
+        </section> 
+        <!-- Final da Seção de Login-->
+        
+        <!-- Formulário de Recuperação de Senha -->
+        <section class="row mt-4">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" 
+            id="caixaLogin">
+                <h2 class="text-center mt-2">
+                    Gerar Nova Senha
+                </h2>
+                <form action="#" method="post" id="formSenha" class="p-2">
+                    <div class="form-gruop">
+                        <small class="text-muted">
+                            Para gerar uma nova senha, digite o
+                            seu e-mail. Clique no link gerado.
+                        </small>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="emailGerarSenha" 
+                        id="emailGerarSenha" class="form-control"
+                        placeholder="E-mail de recuperação de senha">
+                    </div>
+                    
+                    <div class="form-group">
+                        <input type="submit" value="::Gerar::"
+                        name="btnGerar" id="btnGerar"
+                        class="btn btn-primary btn-block">
+                    </div>
+
+                    <div class="form-group">
+                        <p class="text-center">
+                            Já registrado? 
+                            <a href="#" id="btnJaRegistrado">
+                            Entrar por aqui
+                            </a>
+                        </p>
+                    </div>
+
+                </form>
+            </div>
+        </section>
+        <!-- Fim da Seção de Recuperação de Senha -->
+    
     </main>
 
     <!-- Optional JavaScript -->
