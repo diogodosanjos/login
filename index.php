adb3f50e2dca278eb5b9420039de7f704f41f118
Diogo dos Anjos <dodogeremias@gmail.com>
2019-10-01 18:36:23 -0300

    Resolução do problema da aula 03
    index atualizado

--- index.php
+++ index.php
@@ -160,7 +160,7 @@
                     <div class="form-group">
                         <p class="text-center">
                             Já registrado?
-                            <a href="#" id="btnJaRegistrado2">
+                            <a href="#" id="btnRegistrado2">
                                 <!-- Não pode ter 2 ids iguais na página -->
                                 Entrar por aqui
                             </a>
@@ -180,7 +180,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
     <script>
         //Código jQuery para mostrar e ocultar os formulários
         $(function() {
@@ -196,13 +195,14 @@
 
                     $("#btnRegistrarNovo").click(function() {
                         $("#caixaLogin").hide();
-                        $("#caixarRegistro").show();
+                        $("#caixaRegistro").show();
                     });
 
                     $("#btnRegistrado2").click(function() {
                         $("#caixaLogin").show();
-                        $("#caixarRegistro").hide();
+                        $("#caixaRegistro").hide();
                     });
+        });
     </script>
 </body>
 
